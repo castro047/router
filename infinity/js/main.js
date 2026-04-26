4949b3acc0b8f636f427d8b2980c400ca7c09f4d
gustavo <105922570+castro047@users.noreply.github.com>
2026-04-26 16:18:30 -0300

    Add files via upload

---
+++ infinity/js/main.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("faleConoscoBtn").addEventListener("click", function() {
+    const numero = "5545998384668";
+    const mensagem = "olá! vim pelo site Evolux e quero mais informações!";
+    
+    const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+    
+    window.open(url, "_blank");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("footerSaibaBtn").addEventListener("click", function() {
+    const numero = "5545998384668";
+    const mensagem = "olá! vim pelo site Evolux e quero mais informações!";
+    
+    const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+    
+    window.open(url, "_blank");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("credibilidade").addEventListener("click", function() {
+    const numero = "5545998384668";
+    const mensagem = "olá! Quero restaurar minha credibilidade!";
+    
+    const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+    
+    window.open(url, "_blank");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("servicosSaber").addEventListener("click", function() {
+    const numero = "5545998384668";
+    const mensagem = "olá! Quero ver sobre seus Serviços!";
+    
+    const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+    
+    window.open(url, "_blank");
+  });
+});
